05301dccd771c43d57aab9b4dcfd38b3a4c3d7e3
Sohail Nasim <sohailnasim@gmail.com>
2017-10-12 18:09:34 -0400

    test 2

--- ngtest/src/com/sn/test/ng/Planet.java
+++ ngtest/src/com/sn/test/ng/Planet.java
@@ -1,5 +1,19 @@
 package com.sn.test.ng;
 
 public class Planet {
+	private Point point;
 
+	public Planet(Point point) {
+		super();
+		this.point = point;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+	
 }
